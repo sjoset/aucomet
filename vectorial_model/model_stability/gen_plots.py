2570121f2ee9b72940002bb61d7844bc19f336db
Shawn O <szo0032@auburn.edu>
2022-07-13 22:15:12 -0500

    add vectorial model stability testing and plotting code

---
+++ vectorial_model/model_stability/gen_plots.py
@@ -0,0 +1,452 @@
+#!/usr/bin/env python3
+
+import sys
+# import copy
+
+import numpy as np
+# import astropy.units as u
+from astropy.visualization import quantity_support
+import matplotlib.pyplot as plt
+# from matplotlib import cm, colors
+# from matplotlib import colors
+
+__author__ = 'Shawn Oset'
+__version__ = '0.0'
+
+# solarbluecol = np.array([38, 139, 220]) / 255.
+# solarblue = (solarbluecol[0], solarbluecol[1], solarbluecol[2], 1)
+# solargreencol = np.array([133, 153, 0]) / 255.
+# solargreen = (solargreencol[0], solargreencol[1], solargreencol[2], 1)
+# solarblackcol = np.array([0, 43, 54]) / 255.
+# solarblack = (solarblackcol[0], solarblackcol[1], solarblackcol[2], 1)
+# solarwhitecol = np.array([238, 232, 213]) / 255.
+# solarwhite = (solarblackcol[0], solarblackcol[1], solarblackcol[2], 1)
+
+
+def calculated_q_vs_q_plot(all_data, p_tau, f_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given taus
+    pmask = (all_data[:, 1] == p_tau)
+    fmask = (all_data[:, 2] == f_tau)
+    mask = np.logical_and(pmask, fmask)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    cqs = np.log10(all_data[mask, 3])
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig, ax = plt.subplots(figsize=(15, 15))
+
+    # 2d scatter plot
+    ax.scatter(qs, cqs)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Production calculated, log10 Q(H2O)")
+    fig.suptitle(f"Calculated Q vs model input Q, via fixed count in aperture\nParent lifetime {p_tau:06.1f}, fragment lifetime {f_tau:06.1f}")
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def calculated_q_vs_q_plot_3d_fixed_ptau(all_data, p_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given ptaus
+    mask = (all_data[:, 1] == p_tau)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    f_taus = all_data[mask, 2]
+    cqs = np.log10(all_data[mask, 3])
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig = plt.figure(figsize=(30, 30))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    ax.set_zlim([27.2, 29.0])
+    # 3d scatter plot
+    ax.scatter(qs, f_taus, cqs)
+    ax.plot_trisurf(qs, f_taus, cqs, color='white', edgecolors=(0, 0, 0, 0.05), alpha=0.2)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Fragment lifetime, s")
+    ax.set(zlabel="Production calculated, log10 Q(H2O)")
+    fig.suptitle(f"Calculated Q vs model input Q, via fixed count in aperture\nParent lifetime {p_tau:06.1f}")
+
+    # Initialize 3d view at these angles for saving the plot
+    ax.view_init(30, 40)
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def calculated_q_vs_q_plot_3d_fixed_ftau(all_data, f_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given ptaus
+    mask = (all_data[:, 2] == f_tau)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    p_taus = all_data[mask, 1]
+    cqs = np.log10(all_data[mask, 3])
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig = plt.figure(figsize=(30, 30))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    ax.set_zlim([27.2, 29.0])
+    # 3d scatter plot
+    ax.scatter(qs, p_taus, cqs)
+    ax.plot_trisurf(qs, p_taus, cqs, color='white', edgecolors=(0, 0, 0, 0.05), alpha=0.2)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Parent lifetime, s")
+    ax.set(zlabel="Production calculated, log10 Q(H2O)")
+    fig.suptitle(f"Calculated Q vs model input Q, via fixed count in aperture\nFragment lifetime {f_tau:06.1f}")
+
+    # Initialize 3d view at these angles for saving the plot
+    ax.view_init(30, 40)
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def comp_time_vs_q_plot(all_data, p_tau, f_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given taus
+    pmask = (all_data[:, 1] == p_tau)
+    fmask = (all_data[:, 2] == f_tau)
+    mask = np.logical_and(pmask, fmask)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    cts = all_data[mask, 5]
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig, ax = plt.subplots(figsize=(15, 15))
+
+    # 2d scatter plot
+    ax.scatter(qs, cts)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Time for calculation to complete, s")
+    fig.suptitle(f"Model computation time vs model input Q\nParent lifetime {p_tau:06.1f}, fragment lifetime {f_tau:06.1f}")
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def comp_time_vs_q_plot_3d_fixed_ptau(all_data, p_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given ptaus
+    mask = (all_data[:, 1] == p_tau)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    f_taus = all_data[mask, 2]
+    cts = all_data[mask, 5]
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig = plt.figure(figsize=(30, 30))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    # 3d scatter plot
+    ax.scatter(qs, f_taus, cts)
+    ax.plot_trisurf(qs, f_taus, cts, color='white', edgecolors=(0, 0, 0, 0.05), alpha=0.2)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Fragment lifetime, s")
+    ax.set(zlabel="Calculation time, s")
+    fig.suptitle(f"Calculation time vs model input Q\nParent lifetime {p_tau:06.1f}")
+
+    # Initialize 3d view at these angles for saving the plot
+    ax.view_init(30, 40)
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def comp_time_vs_q_plot_3d_fixed_ftau(all_data, f_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given ptaus
+    mask = (all_data[:, 2] == f_tau)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    p_taus = all_data[mask, 1]
+    cts = all_data[mask, 5]
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig = plt.figure(figsize=(30, 30))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    # 3d scatter plot
+    ax.scatter(qs, p_taus, cts)
+    ax.plot_trisurf(qs, p_taus, cts, color='white', edgecolors=(0, 0, 0, 0.05), alpha=0.2)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Parent lifetime, s")
+    ax.set(zlabel="Calculation time, s")
+    fig.suptitle(f"Calculation time vs model input Q\nParent lifetime {f_tau:06.1f}")
+
+    # Initialize 3d view at these angles for saving the plot
+    ax.view_init(30, 40)
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def acc_vs_q_plot(all_data, p_tau, f_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given taus
+    pmask = (all_data[:, 1] == p_tau)
+    fmask = (all_data[:, 2] == f_tau)
+    mask = np.logical_and(pmask, fmask)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    accs = all_data[mask, 4]
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig, ax = plt.subplots(figsize=(15, 15))
+
+    # 2d scatter plot
+    ax.scatter(qs, accs)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Aperture count reproduction accuracy, percent")
+    fig.suptitle(f"Aperture count reproduction accuracy vs model input Q, via fixed count in aperture\nParent lifetime {p_tau:06.1f}, fragment lifetime {f_tau:06.1f}")
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def acc_vs_q_plot_3d_fixed_ptau(all_data, p_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given ptaus
+    mask = (all_data[:, 1] == p_tau)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    f_taus = all_data[mask, 2]
+    accs = all_data[mask, 4]
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig = plt.figure(figsize=(30, 30))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    # 3d scatter plot
+    ax.scatter(qs, f_taus, accs)
+    ax.plot_trisurf(qs, f_taus, accs, color='white', edgecolors=(0, 0, 0, 0.05), alpha=0.2)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Fragment lifetime, s")
+    ax.set(zlabel="Aperture count reproduction accuracy, percent")
+    fig.suptitle(f"Aperture count reproduction accuracy vs model input Q, via fixed count in aperture\nParent lifetime {p_tau:06.1f}")
+
+    # Initialize 3d view at these angles for saving the plot
+    ax.view_init(30, 40)
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def acc_vs_q_plot_3d_fixed_ftau(all_data, f_tau, show_plots=False, out_file=None):
+
+    # select only data rows with given ptaus
+    mask = (all_data[:, 2] == f_tau)
+
+    # first and fourth columns are the data we want
+    qs = np.log10(all_data[mask, 0])
+    p_taus = all_data[mask, 1]
+    accs = all_data[mask, 4]
+
+    plt.style.use('default')
+    plt.style.use('Solarize_Light2')
+
+    fig = plt.figure(figsize=(30, 30))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    # 3d scatter plot
+    ax.scatter(qs, p_taus, accs)
+    ax.plot_trisurf(qs, p_taus, accs, color='white', edgecolors=(0, 0, 0, 0.05), alpha=0.2)
+
+    ax.set(xlabel="Production used in model, log10 Q(H2O)")
+    ax.set(ylabel="Parent lifetime, s")
+    ax.set(zlabel="Aperture count reproduction accuracy, percent")
+    fig.suptitle(f"Aperture count reproduction accuracy vs model input Q, via fixed count in aperture\nFragment lifetime {f_tau:06.1f}")
+
+    # Initialize 3d view at these angles for saving the plot
+    ax.view_init(30, 40)
+
+    if out_file:
+        plt.savefig(out_file)
+    if show_plots:
+        plt.show()
+    plt.close(fig)
+
+
+def do_calc_q_vs_q_plots(all_data):
+
+    p_taus = sorted(set(all_data[:, 1]))
+    f_taus = sorted(set(all_data[:, 2]))
+
+    for pt in p_taus:
+        for ft in f_taus:
+            out_file = f"Calc_Q_vs_Q_ptau_{pt:06.1f}_ftau_{ft:06.1f}.png"
+            print(f"Generating {out_file} ...")
+            calculated_q_vs_q_plot(all_data, pt, ft, out_file=out_file)
+
+
+def do_calc_q_vs_q_fixed_ptau_plots(all_data):
+
+    p_taus = sorted(set(all_data[:, 1]))
+
+    for pt in p_taus:
+        out_file = f"Calc_Q_vs_Q_ptau_{pt:06.1f}_summary.png"
+        print(f"Generating {out_file} ...")
+        calculated_q_vs_q_plot_3d_fixed_ptau(all_data, pt, out_file=out_file)
+
+
+def do_calc_q_vs_q_fixed_ftau_plots(all_data):
+
+    f_taus = sorted(set(all_data[:, 2]))
+
+    for ft in f_taus:
+        out_file = f"Calc_Q_vs_Q_ftau_{ft:06.1f}_summary.png"
+        print(f"Generating {out_file} ...")
+        calculated_q_vs_q_plot_3d_fixed_ftau(all_data, ft, out_file=out_file)
+
+
+def do_comp_time_vs_q_plots(all_data):
+
+    p_taus = sorted(set(all_data[:, 1]))
+    f_taus = sorted(set(all_data[:, 2]))
+
+    for pt in p_taus:
+        for ft in f_taus:
+            out_file = f"comp_time_vs_Q_ptau_{pt:06.1f}_ftau_{ft:06.1f}.png"
+            print(f"Generating {out_file} ...")
+            comp_time_vs_q_plot(all_data, pt, ft, out_file=out_file)
+
+
+def do_comp_time_vs_q_fixed_ptau_plots(all_data):
+
+    p_taus = sorted(set(all_data[:, 1]))
+
+    for pt in p_taus:
+        out_file = f"comp_time_vs_Q_ptau_{pt:06.1f}_summary.png"
+        print(f"Generating {out_file} ...")
+        comp_time_vs_q_plot_3d_fixed_ptau(all_data, pt, out_file=out_file)
+
+
+def do_comp_time_vs_q_fixed_ftau_plots(all_data):
+
+    f_taus = sorted(set(all_data[:, 2]))
+
+    for ft in f_taus:
+        out_file = f"comp_time_vs_Q_ftau_{ft:06.1f}_summary.png"
+        print(f"Generating {out_file} ...")
+        comp_time_vs_q_plot_3d_fixed_ftau(all_data, ft, out_file=out_file)
+
+
+def do_acc_vs_q_plots(all_data):
+
+    p_taus = sorted(set(all_data[:, 1]))
+    f_taus = sorted(set(all_data[:, 2]))
+
+    for pt in p_taus:
+        for ft in f_taus:
+            out_file = f"acc_vs_Q_ptau_{pt:06.1f}_ftau_{ft:06.1f}.png"
+            print(f"Generating {out_file} ...")
+            acc_vs_q_plot(all_data, pt, ft, out_file=out_file)
+
+
+def do_acc_vs_q_fixed_ptau_plots(all_data):
+
+    p_taus = sorted(set(all_data[:, 1]))
+
+    for pt in p_taus:
+        out_file = f"acc_vs_Q_ptau_{pt:06.1f}_summary.png"
+        print(f"Generating {out_file} ...")
+        acc_vs_q_plot_3d_fixed_ptau(all_data, pt, out_file=out_file)
+
+
+def do_acc_vs_q_fixed_ftau_plots(all_data):
+
+    f_taus = sorted(set(all_data[:, 2]))
+
+    for ft in f_taus:
+        out_file = f"acc_vs_Q_ftau_{ft:06.1f}_summary.png"
+        print(f"Generating {out_file} ...")
+        acc_vs_q_plot_3d_fixed_ftau(all_data, ft, out_file=out_file)
+
+
+def main():
+
+    quantity_support()
+
+    with open('output.npdata', 'rb') as np_file:
+        all_data = np.load(np_file)
+
+    # qs = set(all_data[:, 0])
+    # p_taus = sorted(set(all_data[:, 1]))
+    # f_taus = sorted(set(all_data[:, 2]))
+
+    # do_calc_q_vs_q_plots(all_data)
+    # do_calc_q_vs_q_fixed_ptau_plots(all_data)
+    # do_calc_q_vs_q_fixed_ftau_plots(all_data)
+
+    # do_comp_time_vs_q_plots(all_data)
+    # do_comp_time_vs_q_fixed_ptau_plots(all_data)
+    # do_comp_time_vs_q_fixed_ftau_plots(all_data)
+
+    # do_acc_vs_q_plots(all_data)
+    # do_acc_vs_q_fixed_ptau_plots(all_data)
+    do_acc_vs_q_fixed_ftau_plots(all_data)
+
+    # acc_vs_q_plot(all_data, p_taus[0], f_taus[0], show_plots=True)
+    # acc_vs_q_plot_3d_fixed_ptau(all_data, p_taus[0], show_plots=True)
+    # acc_vs_q_plot_3d_fixed_ftau(all_data, f_taus[0], show_plots=True)
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ vectorial_model/model_stability/model_stability.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import copy
+import time
+
+import logging as log
+import numpy as np
+import astropy.units as u
+import matplotlib.pyplot as plt
+from astropy.visualization import quantity_support
+import sbpy.activity as sba
+from argparse import ArgumentParser
+from itertools import product
+
+import pyvectorial as pyv
+
+__author__ = 'Shawn Oset'
+__version__ = '0.1'
+
+
+def process_args():
+
+    # Parse command-line arguments
+    parser = ArgumentParser(
+        usage='%(prog)s [options] [inputfile]',
+        description=__doc__,
+        prog=os.path.basename(sys.argv[0])
+    )
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('--verbose', '-v', action='count', default=0,
+                        help='increase verbosity level')
+    parser.add_argument(
+            'parameterfile', nargs=1, help='YAML file with production and molecule data'
+            )  # the nargs=? specifies 0 or 1 arguments: it is optional
+
+    args = parser.parse_args()
+
+    # handle verbosity
+    if args.verbose >= 2:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+    elif args.verbose == 1:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+
+    return args
+
+
+def file_string_id_from_parameters(vmc):
+
+    base_q = vmc.production.base_q.value
+    p_tau_d = vmc.parent.tau_d.value
+    f_tau_T = vmc.fragment.tau_T.value
+
+    return f"q_{base_q}_ptau_d_{p_tau_d:06.1f}_ftau_T_{f_tau_T:06.1f}"
+
+
+def get_aperture(vmc: pyv.VectorialModelConfig) -> sba.Aperture:
+
+    ap = None
+
+    # get the type
+    ap_type = vmc.etc.get('aperture_type')
+    if ap_type is None:
+        return None
+
+    # get the dimensions as a list and convert to tuple
+    ap_dimensions = tuple(vmc.etc.get('aperture_dimensions'))
+    ap_dimensions = ap_dimensions * u.km
+
+    if ap_type == 'circular':
+        ap = sba.CircularAperture(ap_dimensions)
+    elif ap_type == 'rectangular':
+        ap = sba.RectangularAperture(ap_dimensions)
+    elif ap_type == 'annular':
+        ap = sba.AnnularAperture(ap_dimensions)
+
+    return ap
+
+
+def main():
+
+    """
+        Vary the dummy input production and the water lifetimes and save plotting data
+    """
+
+    quantity_support()
+
+    args = process_args()
+    log.debug("Loading input from %s ....", args.parameterfile[0])
+
+    # Read in our template settings
+    vmc_base = pyv.vm_configs_from_yaml(args.parameterfile[0])[0]
+
+    # sort by production value to group results
+    # vmc_set = sorted(vmc_set, key=lambda vmc: vmc.production.base_q)
+    log_q_begin = 27
+    log_q_end = 31
+    num_qs = 5
+    # num_qs = log_q_end - log_q_begin + 1
+    input_qs = np.logspace(log_q_begin, log_q_end, num=num_qs, endpoint=True) * 1/u.s
+
+    # parent tau_d
+    num_p_tau_ds = 1
+    # num_p_tau_ds = 10
+    p_tau_d_begin = sba.photo_timescale('H2O') / 2
+    p_tau_d_end = sba.photo_timescale('H2O') * 2
+    p_tau_ds = np.linspace(p_tau_d_begin, p_tau_d_end, num=num_p_tau_ds , endpoint=True)
+
+    # fragment tau_T
+    num_f_tau_Ts = 1
+    # num_f_tau_Ts = 10
+    f_tau_T_begin = sba.photo_timescale('OH') / 2
+    f_tau_T_end = sba.photo_timescale('OH') * 2
+    f_tau_Ts = np.linspace(f_tau_T_begin, f_tau_T_end, num=num_f_tau_Ts , endpoint=True)
+
+    total_runs = num_qs * num_p_tau_ds * num_f_tau_Ts
+    runs_completed = 0
+
+    results_list = []
+    for input_q, p_tau, f_tau in product(input_qs, p_tau_ds, f_tau_Ts):
+
+        # set q, p_tau, f_tau for this run
+        vmc = copy.deepcopy(vmc_base)
+        vmc.production.base_q = input_q
+        vmc.parent.tau_d = p_tau
+        vmc.fragment.tau_T = f_tau
+        vmc_check = copy.deepcopy(vmc)
+
+        print(f"{input_q=}, {f_tau=}, {p_tau=}")
+
+        # run the model and track execution time
+        vm_run_t0 = time.time()
+        coma = pyv.run_vmodel(vmc)
+        vm_run_t1 = time.time()
+        vm_run_time = vm_run_t1 - vm_run_t0
+
+        # create aperture from settings in vmc
+        assumed_aperture_count = vmc.etc.get('assumed_aperture_count')
+        ap = get_aperture(vmc)
+
+        # calculate the number of fragments actually in the aperture and what
+        # the production would need to be to produce the assumed count
+        aperture_count = coma.total_number(ap)
+        calculated_q = (assumed_aperture_count / aperture_count) * vmc.production.base_q
+
+        # run another model with this calculated production to see if it actually reproduces
+        # the assumed count
+        vmc_check.production.base_q = calculated_q
+        vm_check_t0 = time.time()
+        coma_check = pyv.run_vmodel(vmc_check)
+        vm_check_t1 = time.time()
+        vm_check_time = vm_check_t1 - vm_check_t0
+        aperture_count_check = coma_check.total_number(ap)
+
+        # how well did we reproduce the assumed count?
+        aperture_accuracy = 100 * aperture_count_check / assumed_aperture_count
+
+        results_list.append([
+            vmc.production.base_q.value, vmc.parent.tau_d.value, vmc.fragment.tau_T.value, 
+            calculated_q.value, aperture_accuracy, vm_run_time, vm_check_time
+            ])
+
+        vmc.etc['aperture_accuracy'] = aperture_accuracy
+        vmc.etc['vmodel_run_time'] = [vm_run_time, vm_check_time]
+        pyv.save_results(vmc, pyv.get_result_from_coma(coma), 'vmout_'+file_string_id_from_parameters(vmc))
+
+        runs_completed += 1
+        print(f"Total progress: {(100 * runs_completed) / total_runs:4.1f} %")
+    
+    results_array = np.array(results_list).reshape((total_runs, 7))
+
+    with open('linear_fits', 'w') as fit_file:
+        for pt in p_tau_ds:
+            for ft in f_tau_Ts:
+                # select only rows with given taus
+                pmask = (results_array[:, 1] == pt.value)
+                fmask = (results_array[:, 2] == ft.value)
+                mask = np.logical_and(pmask, fmask)
+
+                qs = results_array[mask, 0]
+                cqs = results_array[mask, 3]
+                m, b = np.polyfit(qs, cqs, 1)
+                print(f"With p_tau={pt}, f_tau={ft}, q vs. calculated q slope best fit: {m:7.3e} with intercept {b:7.3e}", file=fit_file)
+
+    with open('output.npdata', 'wb') as np_file:
+        np.save(np_file, results_array)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
