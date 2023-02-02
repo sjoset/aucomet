81d4f6980d04ef03494031b2243a714c63c8011f
Shawn O <szo0032@auburn.edu>
2023-02-02 16:06:00 -0600

    vectorial model: improving data generator to make non-water molecule calculations easy

--- vectorial_model/src/data_generator/data_generator.py
+++ vectorial_model/src/data_generator/data_generator.py
@@ -8,13 +8,13 @@ import contextlib
 import multiprocessing
 import warnings
 import time
-import gc
 
 from argparse import ArgumentParser
 from typing import List, Tuple
-from itertools import product
+from itertools import product, groupby
 from astropy.table import vstack, QTable
 from multiprocessing import Pool
+from dataclasses import dataclass
 
 import logging as log
 import importlib.metadata as impm
@@ -24,6 +24,78 @@ import pyvectorial as pyv
 
 __author__ = 'Shawn Oset'
 __version__ = '0.1'
+
+
+def generate_base_vmc_h2o() -> pyv.VectorialModelConfig:
+
+    """
+        Returns vmc with parent and fragment info filled out for water, with generic
+        settings for base_q, r_h, comet name, and high grid settings
+    """
+
+    grid = pyv.Grid(radial_points=150, angular_points=80, radial_substeps=80)
+    comet = pyv.Comet(name='NA', rh=1.0 * u.AU, delta=1.0 * u.AU, transform_method=None, transform_applied=False)
+    production = pyv.Production(base_q=1.e28/u.s, time_variation_type=None, params=None)
+
+    parent = pyv.Parent(
+            name='h2o',
+            # v_outflow=0.85 * u.km/u.s,
+            v_outflow=0.85/np.sqrt((comet.rh.to_value(u.AU))**2) * u.km/u.s,
+            tau_d=86000 * u.s,
+            tau_T=86000 * 0.93 * u.s,
+            sigma=3e-16 * u.cm**2,
+            T_to_d_ratio=0.93
+            )
+
+    fragment = pyv.Fragment(
+            name='oh',
+            v_photo=1.05 * u.km/u.s,
+            tau_T=160000 * u.s
+            )
+
+    etc = {'print_progress': False}
+
+    vmc = pyv.VectorialModelConfig(
+            production=production,
+            parent=parent,
+            fragment=fragment,
+            comet=comet,
+            grid=grid,
+            etc=etc
+            )
+
+    return vmc
+
+
+# TODO: describe what we mean by 'dataset', as in a group of models split up into separate files by AU
+# or make a dataclass for it and rewrite for that
+@dataclass
+class VMParameterSet:
+    parameter_set_id = None
+    rh_list = None
+    base_q_list = None
+    parent_tau_list = None
+    fragment_tau_list = None
+    base_vmc = None
+
+vm_parameter_sets = [
+        # Water, large data set for full analysis
+        VMParameterSet(parameter_set_id = 'h2o',
+                       rh_list = np.linspace(1.0, 4.0, num=13, endpoint=True) * u.AU,
+                       base_q_list = np.logspace(28.0, 30.5, num=31, endpoint=True) / u.s,
+                       parent_tau_list = np.linspace(50000 * u.s, 100000 * u.s, num=10, endpoint=True),
+                       fragment_tau_list = np.linspace(100000 * u.s, 220000 * u.s, num=10, endpoint=True),
+                       base_vmc = generate_base_vmc_h2o()
+                       ),
+        # Water, small data set for testing
+        VMParameterSet(parameter_set_id = 'h2o_small',
+                       rh_list = np.linspace(1.0, 4.0, num=4, endpoint=True) * u.AU,
+                       base_q_list = np.logspace(28.0, 30.5, num=4, endpoint=True) / u.s,
+                       parent_tau_list = np.linspace(50000 * u.s, 100000 * u.s, num=3, endpoint=True),
+                       fragment_tau_list = np.linspace(100000 * u.s, 220000 * u.s, num=3, endpoint=True),
+                       base_vmc = generate_base_vmc_h2o()
+                       )
+        ]
 
 
 def process_args():
@@ -58,12 +130,12 @@ def remove_file_silent_fail(f: pathlib.Path) -> None:
         os.unlink(f)
 
 
-# Service function that takes a vmc, runs a model, and returns results + timing information
-# We map this function over a set of VectorialModelConfigs to get our list of completed models
 def run_vmodel_timed(vmc: pyv.VectorialModelConfig) -> Tuple:
 
     """
-        Return the encoded coma (using the dill library) because python multiprocessing wants
+        Service function that takes a vmc, runs a model, and returns results + timing information.
+
+        Returns the encoded coma (using the dill library) because python multiprocessing wants
         to pickle return values to send them back to the main calling process.  The coma can't be
         pickled by the stock python pickler so we pickle it here with dill and things are fine
     """
@@ -138,52 +210,11 @@ def add_vmc_columns(qt: QTable) -> None:
     qt.add_column([vmc.grid.radial_substeps for vmc in vmc_list], name='radial_substeps')
 
 
-def generate_base_vmc_h2o() -> pyv.VectorialModelConfig:
+def generate_vmc_set_h2o(r_h: u.Quantity) -> List[pyv.VectorialModelConfig]:
 
     """
-        Returns vmc with parent and fragment info filled out for water, with generic
-        settings for base_q, r_h, comet name, and high grid settings
-    """
-
-    grid = pyv.Grid(radial_points=150, angular_points=80, radial_substeps=80)
-    comet = pyv.Comet(name='NA', rh=1.0 * u.AU, delta=1.0 * u.AU, transform_method=None, transform_applied=False)
-    production = pyv.Production(base_q=1.e28/u.s, time_variation_type=None, params=None)
-
-    parent = pyv.Parent(
-            name='h2o',
-            # v_outflow=0.85 * u.km/u.s,
-            v_outflow=0.85/np.sqrt((comet.rh.to_value(u.AU))**2) * u.km/u.s,
-            tau_d=86000 * u.s,
-            tau_T=86000 * 0.93 * u.s,
-            sigma=3e-16 * u.cm**2,
-            T_to_d_ratio=0.93
-            )
-
-    fragment = pyv.Fragment(
-            name='oh',
-            v_photo=1.05 * u.km/u.s,
-            tau_T=160000 * u.s
-            )
-
-    etc = {'print_progress': False}
-
-    vmc = pyv.VectorialModelConfig(
-            production=production,
-            parent=parent,
-            fragment=fragment,
-            comet=comet,
-            grid=grid,
-            etc=etc
-            )
-
-    return vmc
-
-
-def generate_vmc_set_h2o(base_q: u.Quantity, r_h: u.Quantity) -> List[pyv.VectorialModelConfig]:
-
-    """
-        Returns a list of VectorialModelConfigs with the parent and fragment lifetimes
-        varied over a range of interest.  Scalings due to heliocentric distance are then
+        Returns a list of VectorialModelConfigs with base_q varied over 1e28 to 1e30.5
+        and lifetimes varied over a range of interest.  Scalings due to heliocentric distance are then
         applied to lifetimes, and the empirical relation between parent outflow speed
         and r_h is applied as well.
     """
@@ -193,17 +224,19 @@ def generate_vmc_set_h2o(base_q: u.Quantity, r_h: u.Quantity) -> List[pyv.Vector
     base_vmc.comet.rh = r_h
     r_h_AU = r_h.to_value(u.AU)
 
+    base_qs = np.logspace(28.0, 30.5, num=31, endpoint=True) / u.s
+
     # scale lifetimes up by r_h^2
     p_tau_ds = np.linspace(50000 * u.s, 100000 * u.s, num=10, endpoint=True) * r_h_AU**2
     f_tau_Ts = np.linspace(100000 * u.s, 220000 * u.s, num=10, endpoint=True) * r_h_AU**2
 
     vmc_set = []
-    for element in product(p_tau_ds, f_tau_Ts):
+    for element in product(base_qs, p_tau_ds, f_tau_Ts):
         new_vmc = copy.deepcopy(base_vmc)
-        new_vmc.parent.tau_d = element[0]
-        new_vmc.parent.tau_T = element[0] * base_vmc.parent.T_to_d_ratio
-        new_vmc.fragment.tau_T = element[1]
-        new_vmc.production.base_q = base_q
+        new_vmc.production.base_q = element[0]
+        new_vmc.parent.tau_d = element[1]
+        new_vmc.parent.tau_T = element[1] * base_vmc.parent.T_to_d_ratio
+        new_vmc.fragment.tau_T = element[2]
 
         # use empirical formula in CS93 for outflow
         new_vmc.parent.v_outflow = (0.85 / np.sqrt(r_h_AU ** 2)) * u.km/u.s
@@ -213,42 +246,75 @@ def generate_vmc_set_h2o(base_q: u.Quantity, r_h: u.Quantity) -> List[pyv.Vector
     return vmc_set
 
 
-def generate_h2o_fits_file(output_fits_file: pathlib.Path, r_h: u.Quantity, delete_intermediates: bool = False, parallelism=1) -> None:
+def generate_vmc_set_h2o_small(r_h: u.Quantity) -> List[pyv.VectorialModelConfig]:
 
     """
-        Varies base_q over a range to be investigated, generates an output .fits file
-        for each base_q, then combines the .fits files to produce a dataset for water
-        at the given heliocentric distance r_h
+        Returns a list of VectorialModelConfigs with base_q varied over 1e28 to 1e30.5
+        and lifetimes varied over a range of interest.  Scalings due to heliocentric distance are then
+        applied to lifetimes, and the empirical relation between parent outflow speed
+        and r_h is applied as well.
+        Smaller data set for testing purposes
     """
 
-    # To combine the files, the entire dataset needs to be in memory,
-    # so this shouldn't remove the intermediates files and instead check
-    # if they already exist and skip them to so that we can resume calculations
-    # from the last successful sub-calculation
+    base_vmc = generate_base_vmc_h2o()
+
+    base_vmc.comet.rh = r_h
+    r_h_AU = r_h.to_value(u.AU)
+
+    base_qs = np.logspace(28.0, 30.5, num=4, endpoint=True) / u.s
+
+    # scale lifetimes up by r_h^2
+    p_tau_ds = np.linspace(50000 * u.s, 100000 * u.s, num=3, endpoint=True) * r_h_AU**2
+    f_tau_Ts = np.linspace(100000 * u.s, 220000 * u.s, num=3, endpoint=True) * r_h_AU**2
+
+    vmc_set = []
+    for element in product(base_qs, p_tau_ds, f_tau_Ts):
+        new_vmc = copy.deepcopy(base_vmc)
+        new_vmc.production.base_q = element[0]
+        new_vmc.parent.tau_d = element[1]
+        new_vmc.parent.tau_T = element[1] * base_vmc.parent.T_to_d_ratio
+        new_vmc.fragment.tau_T = element[2]
+
+        # use empirical formula in CS93 for outflow
+        new_vmc.parent.v_outflow = (0.85 / np.sqrt(r_h_AU ** 2)) * u.km/u.s
+
+        vmc_set.append(new_vmc)
+
+    return vmc_set
+
+
+def generate_fits_file(output_fits_file: pathlib.Path, r_h: u.Quantity, vmc_list: List[pyv.VectorialModelConfig], delete_intermediates: bool = False, parallelism=1) -> None:
+
+    """
+        Compute all of the models at a given AU and generate a (probably large) fits file with all of the results.
+
+    """
 
     # list of PurePath filenames of intermediate files
     out_file_list = []
     # list of intermediate QTable objects
     out_table_list = []
 
-    qs = np.logspace(28.0, 30.5, num=31, endpoint=True) / u.s
-    for i, base_q in enumerate(qs):
-        percent_complete = (i * 100)/len(qs)
+    # vmc_list = vmc_list_generator(r_h=r_h)
+
+    # split our set of vmc into sublists, where every sublist has a matching base_q
+    # so we can loop through and compute all the models that have the same base_q and save the intermediate results
+    grouped_by_base_q = [list(x) for _, x in groupby(vmc_list, lambda x: x.production.base_q)]
+
+    # build one intermediate output table for each base_q and combine after all of the calculations are done
+    for i, vmc_sublist in enumerate(grouped_by_base_q):
+        percent_complete = (i * 100)/len(grouped_by_base_q)
+        base_q = vmc_sublist[0].production.base_q
+
         print(f"{r_h.to_value(u.AU)} AU\t\tq: {base_q:3.1e}\t\t{percent_complete:4.1f} %")
 
-        vmc_set = generate_vmc_set_h2o(base_q, r_h=r_h)
-# <<<<<<< HEAD
         out_filename = pathlib.Path(output_fits_file.stem + '_' + str(base_q.to_value(1/u.s)) + '.fits')
         if out_filename.is_file():
             print(f"Found intermediate file {out_filename}, skipping generation and reading file instead...")
             out_table = QTable.read(out_filename, format='fits')
         else:
-            out_table = build_calculation_table(vmc_set, parallelism=parallelism)
+            out_table = build_calculation_table(vmc_sublist, parallelism=parallelism)
 
-# =======
-#         out_table = build_calculation_table(vmc_set, parallelism=parallelism)
-#         out_filename = pathlib.Path(output_fits_file.stem + '_' + str(base_q.to_value(1/u.s)) + '.fits')
-# >>>>>>> c45da74 (Vectorial model: find Haser model parameters through fitting given column density data, test version of model with no collision sphere)
         out_file_list.append(out_filename)
 
         log.info("Table building for base production %s complete, writing results to %s ...", base_q, out_filename)
@@ -270,84 +336,105 @@ def generate_h2o_fits_file(output_fits_file: pathlib.Path, r_h: u.Quantity, dele
     del final_table
 
 
-def make_h2o_dataset_table() -> QTable:
+def make_dataset_table(molecule_name: str, au_list: List[np.float64]) -> QTable:
 
     """
-        Defines a list of water dataset files at a range of heliocentric distances, from 1 AU to 4 AU.
+        Defines a list of dataset files at a range of heliocentric distances given by au_list
         Returns an astropy QTable with columns:
             | r_h | filename | whether 'filename' already exists |
         We use this to track which data still needs to be generated, and allows different r_h datasets to
         be generated in different sessions in case of power outages etc.
     """
 
-    aus = np.linspace(1.0, 4.0, num=13, endpoint=True)
-    output_fits_filenames = [pathlib.Path(f"h2o_{au:3.2f}_au.fits") for au in aus]
+    # aus = np.linspace(1.0, 4.0, num=13, endpoint=True)
+    output_fits_filenames = [pathlib.Path(f"{molecule_name}_{au:3.2f}_au.fits") for au in au_list]
     fits_file_exists = [a.is_file() for a in output_fits_filenames]
 
-    return QTable([aus * u.AU, output_fits_filenames, fits_file_exists], names=('r_h', 'filename', 'exists'),
-                  meta={'parent_molecule': 'h2o'})
+    return QTable([au_list * u.AU, output_fits_filenames, fits_file_exists], names=('r_h', 'filename', 'exists'),
+                  meta={'parent_molecule': molecule_name})
+
+
+def get_dataset_selection() -> Tuple:
+
+    """
+        Allows the user to select a dataset and returns a tuple with the relevant parameters for
+        the selected dataset
+    """
+
+    selections = [('h2o', np.linspace(1.0, 4.0, num=13, endpoint=True), generate_vmc_set_h2o),
+                  ('h2o_small', np.linspace(1.0, 4.0, num=4, endpoint=True), generate_vmc_set_h2o_small)]
+
+    dataset_names = [x[0] for x in selections]
+    user_selection = None
+
+    while user_selection == None:
+        print("Select a dataset:")
+        for i, dataset_name in enumerate(dataset_names):
+            print(f"{i}:\t{dataset_name}")
+
+        raw_selection = input()
+        try:
+            selection = int(raw_selection)
+        except ValueError:
+            print("Numbers only, please")
+            selection = -1
+
+        if selection in range(len(dataset_names)):
+            user_selection = selection
+
+    return selections[user_selection]
 
 
 def main():
+
+    # TODO: make a dataclass instead of using dataset_table?
 
     # suppress warnings cluttering up the output
     warnings.filterwarnings("ignore")
     process_args()
 
-    # generate_h2o_fits_file_small(pathlib.Path('h2osmall_2au.fits'), r_h=2.0 * u.AU, delete_intermediates=True, parallelism=4)
-
     # figure out how parallel the model running can be, leaving one core open (unless there's only a single core)
     parallelism = max(1, multiprocessing.cpu_count()-1)
     print(f"Max CPUs: {multiprocessing.cpu_count()}\tWill use: {parallelism} concurrent processes")
 
-    # TODO:
-    # This can probably be generic because we should just be filling in a table,
-    # so this could be a function that takes the dataset table generating function (make_h2o_dataset_table, etc.)
-    # and use that to fill in the table as requested, assuming all of our datasets are varied over r_h in some way
+    # we define a few datasets of interest to us - which one to compute?
+    molecule_name, au_list, vmc_list_generator = get_dataset_selection()
+    dataset_table = make_dataset_table(molecule_name=molecule_name, au_list=au_list)
 
-    # TODO:
-    # This 'water section' could also be main() from another file as a plugin type of architecture, where we just discover all the
-    # dataset_*.py files and ask which one to do
-    """
-        Water section
-    """
-    h2o_dataset_table = make_h2o_dataset_table()
-
-    print(f"Datasets for {h2o_dataset_table.meta['parent_molecule']}:")
-    print(h2o_dataset_table)
+    print(f"Datasets for {dataset_table.meta['parent_molecule']}:")
+    print(dataset_table)
 
     # fill generate_now column with False and ask which data to generate, if any
-    h2o_dataset_table.add_column(False, name='generate_now')
+    dataset_table.add_column(False, name='generate_now')
 
     # do all of them, or pick which ones?
-    if input(f"Generate all missing data for {h2o_dataset_table.meta['parent_molecule']}? [N/y] ") in ['y', 'Y']:
-        for row in h2o_dataset_table:
+    if input(f"Generate all missing data for {dataset_table.meta['parent_molecule']}? [N/y] ") in ['y', 'Y']:
+        # mark all missing data to be generated this run
+        for row in dataset_table:
+            # if it exists, don't generate, and vice versa
             row['generate_now'] = not row['exists']
     else:
-        for row in h2o_dataset_table:
+        # pick which missing data to run
+        for row in dataset_table:
             if row['exists']:
                 continue
             if input(f"Generate data for {row['r_h']}? [N/y] ") in ['y', 'Y']:
                 row['generate_now'] = True
                 print("OK")
 
-    if not any([row['generate_now'] for row in h2o_dataset_table]):
+    if not any([row['generate_now'] for row in dataset_table]):
         print("No data selected for generation!  Quitting.")
         return
 
     print("Current settings:")
-    print(h2o_dataset_table)
+    print(dataset_table)
     if input("Calculate? [N/y] ") not in ['y', 'Y']:
         print("Quitting.")
         return
 
-    for row in h2o_dataset_table:
+    for row in dataset_table:
         if row['generate_now']:
-            generate_h2o_fits_file(row['filename'], row['r_h'], delete_intermediates=False, parallelism=parallelism)
-            # call the garbage collector to clean up between generating data files
-            # Not sure if this works but it might help the script finish if we generate all the data at once
-            # TODO: figure out if we need this, try it with smaller dataset?
-            print(f"Cleaning up: garbage collected {gc.collect()} items.")
+            generate_fits_file(row['filename'], row['r_h'], vmc_list=vmc_list_generator(row['r_h']), delete_intermediates=False, parallelism=parallelism)
 
 
 if __name__ == '__main__':
